b0c2c6f5189fe07fc4c5ab5c5adc6d4d830b62a9
Prags1709 <prags1709@gmail.com>
2022-09-20 19:37:16 +0530

    code fixed

--- prime.js
+++ prime.js
@@ -1,12 +1,12 @@
 //Problem 3: Use the same function to print Non-Primes from 2 to a given limit
 function findPrime(num) {
-  let factors = 0;
+  let count = 0;
   for (i = 1; i <= num; i++) {
     if (num % i == 0) {
-      factors++;
+      count++;
     }
   }
-  if (factors == 2) {
+  if (count == 2) {
     return true;
   }
   return false;
